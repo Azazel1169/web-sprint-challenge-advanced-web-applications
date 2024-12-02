d29ac6505026d03b9b0c280ccc65d0e182a0c00d
Josh Rime <joshrime1990@icloud.com>
2024-12-02 01:03:08 -0600

    fixed error

--- frontend/components/Spinner.test.js
+++ frontend/components/Spinner.test.js
@@ -2,7 +2,7 @@
 // that it renders what it should for the different props it can take.
 import React from "react"
 import {render} from '@testing-library/react'
-import '@testing-library/jest-dom/extend-expect'; 
+import '@testing-library/jest-dom'; 
 import Spinner from './Spinner'; 
 
 test("renders spinner when 'on' prop is true", () => {
